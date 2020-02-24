ac026d46f881d7dfe30c08dcf9b9e94a575c13bf
abufadl <61349083+abufadl@users.noreply.github.com>
2020-02-24 12:24:08 +0200

    Update server.py

--- server.py
+++ server.py
@@ -17,23 +17,6 @@ import requests
 #import nest_asyncio
 #nest_asyncio.apply()
 
-# needed to load learner 
-@np_func
-def f1(inp,targ): return f1_score(targ, np.argmax(inp, axis=-1), average='weighted')
-
-class WeightedLabelSmoothingCrossEntropy(nn.Module):
-    def __init__(self, weight, eps:float=0.1, reduction='mean'):
-        super().__init__()
-        self.weight,self.eps,self.reduction = weight,eps,reduction
-        
-    def forward(self, output, target):
-        c = output.size()[-1]
-        log_preds = F.log_softmax(output, dim=-1)
-        if self.reduction=='sum': loss = -log_preds.sum()
-        else:
-            loss = -log_preds.sum(dim=-1)
-            if self.reduction=='mean':  loss = loss.mean()
-        return loss*self.eps/c + (1-self.eps) * F.nll_loss(log_preds, target, weight=self.weight, reduction=self.reduction)
         
 #!mkdir -p /root/.fastai/data/arwiki/corpus2_100/tmp/
 #data_path = Config.data_path()
@@ -90,6 +73,25 @@ def setup_learner():
 #learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 #loop.run_forever()
 #loop.close()
+
+# needed to load learner 
+@np_func
+def f1(inp,targ): return f1_score(targ, np.argmax(inp, axis=-1), average='weighted')
+
+class WeightedLabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, weight, eps:float=0.1, reduction='mean'):
+        super().__init__()
+        self.weight,self.eps,self.reduction = weight,eps,reduction
+        
+    def forward(self, output, target):
+        c = output.size()[-1]
+        log_preds = F.log_softmax(output, dim=-1)
+        if self.reduction=='sum': loss = -log_preds.sum()
+        else:
+            loss = -log_preds.sum(dim=-1)
+            if self.reduction=='mean':  loss = loss.mean()
+        return loss*self.eps/c + (1-self.eps) * F.nll_loss(log_preds, target, weight=self.weight, reduction=self.reduction)
+    
 learn = setup_learner()
 
 
@@ -150,5 +152,24 @@ def form(request):
     
     
 if __name__ == '_main__':
+    
+    # needed to load learner 
+    @np_func
+    def f1(inp,targ): return f1_score(targ, np.argmax(inp, axis=-1), average='weighted')
+
+    class WeightedLabelSmoothingCrossEntropy(nn.Module):
+        def __init__(self, weight, eps:float=0.1, reduction='mean'):
+            super().__init__()
+            self.weight,self.eps,self.reduction = weight,eps,reduction
+
+        def forward(self, output, target):
+            c = output.size()[-1]
+            log_preds = F.log_softmax(output, dim=-1)
+            if self.reduction=='sum': loss = -log_preds.sum()
+            else:
+                loss = -log_preds.sum(dim=-1)
+                if self.reduction=='mean':  loss = loss.mean()
+            return loss*self.eps/c + (1-self.eps) * F.nll_loss(log_preds, target, weight=self.weight, reduction=self.reduction)
+    
     port1 = int(os.environ.get('PORT', 5000))
     uvicorn.run(app, host='0.0.0.0', port=port1, reload = True)
