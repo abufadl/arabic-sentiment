ec85897639d5d3cb9daae24b0b20ddd922db426e
abufadl <61349083+abufadl@users.noreply.github.com>
2020-02-25 20:58:44 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -103,7 +103,7 @@ async def classify(request):
     #print(str(prediction))
     # try to log activity ................ stdout probably goes to browser, not log
     #sys.stdout.write(f'Entry: {text_data}, Prediction: {prediction}')
-    sys.stderr.write(f'Client: {request.client.host}, Entry: {text_data}, Prediction: {prediction}\n')
+    sys.stderr.write(f'Client: {request.client}, Entry: {text_data}, Prediction: {prediction}\n')
 
     probs = [{ 'class': classes[i], 'probability': round(prediction[2][i].item(),5) } for i in range(len(prediction[2]))]
 
